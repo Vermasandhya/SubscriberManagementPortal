978b4a9a5bfe1e0f365121b2ec973680bdeb2e19
harinielumalai0311 <harini.elumalai03112004@gmail.com>
2025-08-31 11:31:49 +0530

    Delete backend directory

--- backend/PlanService/src/main/java/com/subscriber/planservice/repository/PlanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.subscriber.planservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.subscriber.planservice.entity.Plan;
-
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
--- backend/SubscrptionService/src/main/java/com/subscriber/subscriptionservice/repository/SubscriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.subscriber.subscriptionservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.subscriber.subscriptionservice.entity.Subscription;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-}
